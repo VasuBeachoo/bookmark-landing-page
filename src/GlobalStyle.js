bf234f4cc853d249346351eb764b7a3806d70864
VasuBeachoo <vasubeachoo@gmail.com>
2022-09-21 15:46:28 -0400

    Define colors and fonts

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,8 +3,16 @@ import { createGlobalStyle, css } from "styled-components";
 const styled = { createGlobalStyle };
 
 const GlobalStyle = styled.createGlobalStyle`
+  :root {
+    --Soft-blue: hsl(231, 69%, 60%);
+    --Soft-red: hsl(0, 94%, 66%);
+    --Grayish-blue: hsl(229, 8%, 60%);
+    --Dark-blue: hsl(229, 31%, 21%);
+  }
+
   * {
     box-sizing: border-box;
+    font-family: "Rubik", sans-serif;
   }
 
   body {
